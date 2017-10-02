714c0d939af1475197b23da0afcf44a8042c609f
Jacek Sadowski <testy.szkolenia@gmail.com>
2017-10-03 00:05:50 +0200

    dodano nową stronę INFO

--- source/app.js
+++ source/app.js
@@ -17,6 +17,11 @@ app.get('/services', function(req, res){
 	console.log('Zażądano strony Services');
 });
 
+app.get('/info', function(req, res){
+	res.send('DevOps - strona INFOS!');
+	console.log('Zażądano strony INFOS');
+});
+
 app.listen(3001, function(){
 	console.log('Aplikacja OK');
 });
